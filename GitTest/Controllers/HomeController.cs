40c285b8f58c802d0a8c9682ad03d68c7ccf08cd
Ravi Narsini <ravi.narsi@ibridgetechsoft.com>
2022-11-21 11:48:48 +0530

    Added appointment action

--- GitTest/Controllers/HomeController.cs
+++ GitTest/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace GitTest.Controllers
             return View();
         }
 
+        public ActionResult Create()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
